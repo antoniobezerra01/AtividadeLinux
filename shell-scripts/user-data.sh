91248e34ced9e41384ed89cacdc9d1b6d7cccf5c
Antonio Carlos <antonio.bezerra1414@gmail.com>
2023-01-22 13:50:40 +0000

    Configurando git no user-data

--- shell-scripts/user-data.sh
+++ shell-scripts/user-data.sh
@@ -5,8 +5,13 @@ sudo yum update -y
 
 # Baixando e configurando git para versionamento
 yum install -y git
+cd /
+git clone https://github.com/antoniobezerra01/AtividadeLinux.git 
 
 # Baixando e iniciando apache
 yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
+
+# Configurando script valida-status-httpd
+/bin/cp -f /AtividadeLinux/etc/crontab /etc/crontab
