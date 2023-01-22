f4d4ddb3b691c6d0a39b00b9f6e49cdab52f5fa1
Antonio Carlos <antonio.bezerra1414@gmail.com>
2023-01-22 13:06:33 +0000

    Atualização no script valida-status-httpd

--- shell-scripts/valida-status-httpd.sh
+++ shell-scripts/valida-status-httpd.sh
@@ -5,16 +5,21 @@ SERVICO=httpd
 STATUS="$(systemctl is-active ${SERVICO})"
 
 # Diretorio o qual será usado para log
-mkdir /antonio-carlos/
+# ! -> Operador de negação
+# -d diretorio -> opção do comando test para verficar se diretório existe
+# Verificando se o diretório /antonio-carlos existe
+if [ ! -d /antonio-carlos ]; then
+	mkdir /antonio-carlos/
+fi
 
 # Verificando se o serviço do Apache está ativo ou inativo
 # Gerando log sobre o status do httpd
 if [ "${STATUS}" = "active" ]; then
 	date >> /antonio-carlos/ativo.txt
 	echo "status=ativo" >> /antonio-carlos/ativo.txt
-	echo -e "O serviço httpd está online\n" >> /antonio-carlos/ativo.txt 
+	echo -e "O serviço ${SERVICO} está online\n" >> /antonio-carlos/ativo.txt 
 elif [ "${STATUS}" = "inactive" ]; then
 	date >> /antonio-carlos/inativo.txt
 	echo "status=inativo" >> /antonio-carlos/inativo.txt
-	echo -e  "O serviço httpd está inativo\n" >> /antonio-carlos/inativo.txt
+	echo -e  "O serviço ${SERVICO} está inativo\n" >> /antonio-carlos/inativo.txt
 fi
