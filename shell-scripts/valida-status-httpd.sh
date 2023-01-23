d9afea1a98c6302ba2f5e6db232dbd13728c2e26
Antonio Bezerra <antonio.bezerra1414@gmail.com>
2023-01-23 13:28:13 -0300

    Modificando pagina de log no script

--- shell-scripts/valida-status-httpd.sh
+++ shell-scripts/valida-status-httpd.sh
@@ -7,19 +7,19 @@ STATUS="$(systemctl is-active ${SERVICO})"
 # Diretorio o qual será usado para log
 # ! -> Operador de negação
 # -d diretorio -> opção do comando test para verficar se diretório existe
-# Verificando se o diretório /antonio-carlos existe
-if [ ! -d /antonio-carlos ]; then
-	mkdir /antonio-carlos/
+# Verificando se o diretório /nfs existe
+if [ ! -d /mnt/nfs ]; then
+	mkdir /mnt/nfs/
 fi
 
 # Verificando se o serviço do Apache está ativo ou inativo
 # Gerando log sobre o status do httpd
 if [ "${STATUS}" = "active" ]; then
-	date >> /antonio-carlos/ativo.txt
-	echo "status=ativo" >> /antonio-carlos/ativo.txt
-	echo -e "O serviço ${SERVICO} está online\n" >> /antonio-carlos/ativo.txt 
+	date >> /mnt/nfs/antonio-carlos/ativo.txt
+	echo "status=ativo" >> /mnt/nfs/antonio-carlos/ativo.txt
+	echo -e "O serviço ${SERVICO} está online\n" >>/mnt/nfs/antonio-carlos/ativo.txt 
 elif [ "${STATUS}" = "inactive" ]; then
-	date >> /antonio-carlos/inativo.txt
-	echo "status=inativo" >> /antonio-carlos/inativo.txt
-	echo -e  "O serviço ${SERVICO} está inativo\n" >> /antonio-carlos/inativo.txt
+	date >> /mnt/nfs/antonio-carlos/inativo.txt
+	echo "status=inativo" >> /mnt/nfs/antonio-carlos/inativo.txt
+	echo -e  "O serviço ${SERVICO} está inativo\n" >> /mnt/nfs/antonio-carlos/inativo.txt
 fi
